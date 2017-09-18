a4a027bbd073cd7cd51ff470a686639c7f83ae1e
nasseralnuaimi <31886003+nasseralnuaimi@users.noreply.github.com>
2017-09-18 08:46:30 +0400

    Improvements
    Improvements based on Module 5 small project review.
    
    - redone the tests
    - added withdraw

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.6;
 
-contract Splitter {
+/*contract Splitter {
     
     address public bob;
     address public carol;
@@ -25,5 +25,58 @@ contract Splitter {
          
          LogSplit(bob,carol,msg.value);
          
+    }
+}
+*/
+
+pragma solidity ^0.4.6;
+
+contract Splitter {
+    
+    address public owner;
+
+    mapping(address => uint) public accounts;
+    
+    
+    event LogSplit(address bob, address carol, uint amount);
+    event LogWithdraw(address reciever, uint amount);
+    event LogKilled(address sender);
+    
+    function Splitter() public{
+        owner = msg.sender;
+    }
+    
+    function split(address b,address c) payable public returns (bool sucess){
+         require(msg.value > 0);
+         require(b != 0);
+         require(c != 0);
+
+         accounts[b] += msg.value/2;
+         accounts[c] += msg.value - msg.value/2;
+         
+         LogSplit(b,c,msg.value/2);
+         return true;
+         
+    }
+
+    function withdraw() public returns (bool sucess){
+        require(accounts[msg.sender] > 0);
+
+        var balance = accounts[msg.sender];
+        accounts[msg.sender] -= balance;
+
+        msg.sender.transfer(balance);
+
+        LogWithdraw(msg.sender,balance);
+        return true;
+
+    }
+    function kill() returns (bool sucess){
+         require(msg.sender == owner);
+         selfdestruct(msg.sender);
+
+         LogKilled(owner);
+         return true;
+
     }
 }
--- test/Splitter.js
+++ test/Splitter.js
@@ -1,6 +1,6 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
-contract('Splitter', function(accounts) {
+/*contract('Splitter', function(accounts) {
   var contract;
   var alice = accounts[0];
   var bob   = accounts[1];
@@ -48,4 +48,80 @@ contract('Splitter', function(accounts) {
       assert.equal(carolBefore.plus(6).toString(10), carolAfter.toString(10), "Wrong Balance for Carol");
      });
   });
+
+  it("should not split nothing", function(){
+    var bobBefore = web3.eth.getBalance(bob);
+    var carolBefore = web3.eth.getBalance(carol);
+    return contract.split({from: alice, value:0})
+      then(function(txn){
+      var bobAfter = web3.eth.getBalance(bob);
+      var carolAfter = web3.eth.getBalance(carol);
+      assert.equal(bobBefore.toString(10), bobAfter.toString(10), "Wrong Balance for Bob");
+      assert.equal(carolBefore.toString(10), carolAfter.toString(10), "Wrong Balance for Carol");
+     });
+  });
 });
+*/
+
+contract('Splitter', function(accounts) {
+   var contract;
+   var owner = accounts[0];
+   var bob = accounts[1];
+   var carol = accounts[2];
+
+   beforeEach(function() {
+    return Splitter.new({from: owner})
+    .then(function(instance){
+      contract = instance;
+    });
+  });
+
+   it("should just say hi", function(){
+    assert.strictEqual(true,true,"something is wrong");
+  });
+
+   it("should split Even equally", function(){
+    var bobBefore = web3.eth.getBalance(bob);
+    var carolBefore = web3.eth.getBalance(carol);
+    return contract.split(bob, carol, {from: owner, value:10})
+      .then(function(txn){
+      var bobAfter = web3.eth.getBalance(bob);
+      var carolAfter = web3.eth.getBalance(carol);
+      assert.equal(bobBefore.plus(5).toString(10), bobAfter.toString(10), "Wrong Balance for Bob");
+      assert.equal(carolBefore.plus(5).toString(10), carolAfter.toString(10), "Wrong Balance for Carol");
+     });
+  });
+
+   it("should split Odd properly", function(){
+    var bobBefore = web3.eth.getBalance(bob);
+    var carolBefore = web3.eth.getBalance(carol);
+    return contract.split(bob, carol,{from: owner, value:11})
+      .then(function(txn){
+      var bobAfter = web3.eth.getBalance(bob);
+      var carolAfter = web3.eth.getBalance(carol);
+      assert.equal(bobBefore.plus(5).toString(10), bobAfter.toString(10), "Wrong Balance for Bob");
+      assert.equal(carolBefore.plus(6).toString(10), carolAfter.toString(10), "Wrong Balance for Carol");
+     });
+  });
+
+   it("should only allow withdraw from account owner", function(){
+    contract.split(bob, carol,{from: owner, value:10})
+    contract.withdraw({from: carol})
+    .then(function(balance){
+      assert.equal(contract.accounts(carol).toString("10"),5,"Wrong Balance for Carol");
+      return contract.accounts(carol);
+    });
+    contract.withdraw({from: bob})
+    .then(function(balance){
+      assert.equal(contract.accounts(bob).toString("10"),5,"Wrong Balance for Bob");
+      return contract.accounts(bob);
+    });
+   });
+ });
+
+
+   /*
+       .then(function(txHash){
+      assert.isNotNull(txHash, "Withdraw was not mined");
+    });
+    */
